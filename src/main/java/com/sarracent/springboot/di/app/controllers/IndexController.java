4614abc01fc55713318b595bb3682ea174015c9c
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-03-29 19:39:19 -0300

    Add dependency injection via constructor

--- src/main/java/com/sarracent/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/sarracent/springboot/di/app/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.sarracent.springboot.di.app.controllers;
 
+import com.sarracent.springboot.di.app.models.service.IServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private IServicio miServicio;
+
+    @Autowired
+    public IndexController(IServicio miServicio) {
+        this.miServicio = miServicio;
+    }
+
     @GetMapping({"/", "", "/index"})
     public String index(Model model){
+        model.addAttribute("objeto", miServicio.operacion());
         return "index";
     }
 
---
+++ src/main/java/com/sarracent/springboot/di/app/models/service/IServicio.java
@@ -0,0 +1,6 @@
+package com.sarracent.springboot.di.app.models.service;
+
+public interface IServicio {
+
+    public String operacion();
+}
