8ebcd3edd7298c5e132a984c4806e6d28ee80a35
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-03-31 15:51:46 -0300

    Add domain

--- src/main/java/com/sarracent/springboot/di/app/AppConfig.java
+++ src/main/java/com/sarracent/springboot/di/app/AppConfig.java
@@ -1,5 +1,7 @@
 package com.sarracent.springboot.di.app;
 
+import com.sarracent.springboot.di.app.models.domain.ItemFactura;
+import com.sarracent.springboot.di.app.models.domain.Producto;
 import com.sarracent.springboot.di.app.models.service.IServicio;
 import com.sarracent.springboot.di.app.models.service.MiServicio;
 import com.sarracent.springboot.di.app.models.service.MiServicioComplejo;
@@ -7,10 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.util.Arrays;
+import java.util.List;
+
 //Cuando queremos inyectar clases de 3ros
 @Configuration
 public class AppConfig {
-
 
     @Bean("miServicioSimple")
     public IServicio registrarMiServicio(){
@@ -21,6 +25,18 @@ public class AppConfig {
     @Bean("miServicioComplejo")
     public IServicio registrarMiServicioComplejo(){
         return new MiServicioComplejo();
+    }
+
+    @Bean("itemsFactura")
+    public List<ItemFactura> registrarItems() {
+
+        Producto producto1 = new Producto("Pan", 20);
+        Producto producto2 = new Producto("carne", 100);
+
+        ItemFactura linea1 = new ItemFactura(producto1, 4);
+        ItemFactura linea2 = new ItemFactura(producto2, 2);
+
+        return Arrays.asList(linea1, linea2);
     }
 
 }
---
+++ src/main/java/com/sarracent/springboot/di/app/models/domain/ItemFactura.java
@@ -0,0 +1,32 @@
+package com.sarracent.springboot.di.app.models.domain;
+
+public class ItemFactura {
+
+    private Producto producto;
+
+    private Integer cantidad;
+
+    public ItemFactura() {
+    }
+
+    public ItemFactura(Producto producto, Integer cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
